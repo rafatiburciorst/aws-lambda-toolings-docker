590ee25bcc503019f444bfa618f80a41620708ef
rafatiburciorst <rafael.tiburcio@live.com>
2023-08-06 09:05:07 -0300

    chore: setup aws local

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { S3 } = require("./factory");
+
+module.exports.handler = async (event) => {
+  const allBuckets = await S3.listBuckets().promise()
+  console.log('findAllBuckets ', allBuckets);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        allBuckets
+      },
+      null,
+      2
+    ),
+  };
+};
